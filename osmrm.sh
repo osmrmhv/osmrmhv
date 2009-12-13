ed4e0a97155af79219054017ea2204f3699996e2
Candid Dauth <cdauth@cdauth.de>
2009-12-13 20:52:54 +0100

    Made osmhv and osmrm shell scripts work again.

---
+++ osmrm.sh
@@ -0,0 +1,2 @@
+dir="$(dirname "$(readlink -m "$(which "$0")")")"
+java -Xmx256M -classpath "$dir/lib/sqlite-jdbc/sqlite-jdbc-3.6.14.1.jar:$dir/lib/cmdargs/dist_src/cmdargs.jar:$dir/dist/osmrmhv.jar" de.cdauth.osm.routemanager.Main "$@"
