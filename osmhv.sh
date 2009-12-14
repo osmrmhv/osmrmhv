84f5b8c872f98228a3ee729af301d5c9beefe917
Candid Dauth <cdauth@cdauth.de>
2009-12-14 19:28:15 +0100

    Added the possibility to build dynamic executables (ant gcj-shared).

--- osmhv.sh
+++ osmhv.sh
@@ -1,2 +1,7 @@
 dir="$(dirname "$(readlink -m "$(which "$0")")")"
-java -Xmx256M -classpath "$dir/lib/sqlite-jdbc/sqlite-jdbc-3.6.14.1.jar:$dir/lib/cmdargs/dist/cmdargs.jar:$dir/dist/osmrmhvlib.jar:$dir/dist/osmhv.jar" de.cdauth.osm.historyviewer.Main "$@"
+
+if [ -f "$dir/osmhv" ]; then
+	LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$dir/dist:$dir/lib/cmdargs/dist:$dir/lib/sqlite-jdbc" "$dir/osmhv" "$@"
+else
+	java -Xmx256M -classpath "$dir/lib/sqlite-jdbc/sqlite-jdbc-3.6.14.1.jar:$dir/lib/cmdargs/dist/cmdargs.jar:$dir/dist/osmrmhvlib.jar:$dir/dist/osmhv.jar" de.cdauth.osm.historyviewer.Main "$@"
+fi
--- osmrm.sh
+++ osmrm.sh
@@ -1,2 +1,6 @@
 dir="$(dirname "$(readlink -m "$(which "$0")")")"
-java -Xmx256M -classpath "$dir/lib/sqlite-jdbc/sqlite-jdbc-3.6.14.1.jar:$dir/lib/cmdargs/dist/cmdargs.jar:$dir/dist/osmrmhvlib.jar:$dir/dist/osmrm.jar" de.cdauth.osm.routemanager.Main "$@"
+if [ -f "$dir/osmrm" ]; then
+	LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$dir/dist:$dir/lib/cmdargs/dist:$dir/lib/sqlite-jdbc" "$dir/osmrm" "$@"
+else
+	java -Xmx256M -classpath "$dir/lib/sqlite-jdbc/sqlite-jdbc-3.6.14.1.jar:$dir/lib/cmdargs/dist/cmdargs.jar:$dir/dist/osmrmhvlib.jar:$dir/dist/osmrm.jar" de.cdauth.osm.routemanager.Main "$@"
+fi
