0ecb90cf73264504af084ce43f3ea79b99dda0c3
Candid Dauth <cdauth@cdauth.de>
2010-01-01 17:18:37 +0100

    Removed History Viewer and webapp stuff.
    History Viewer will be developed in its own repository, the web app will
    be there, too.

--- osmhv.sh
+++
@@ -1,7 +0,0 @@
-dir="$(dirname "$(readlink -m "$(which "$0")")")"
-
-if [ -f "$dir/osmhv" ]; then
-	LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$dir/dist:$dir/lib/cmdargs/dist:$dir/lib/sqlite-jdbc" "$dir/osmhv" "$@"
-else
-	java -Xmx256M -classpath "$dir/lib/sqlite-jdbc/sqlite-jdbc-3.6.14.1.jar:$dir/lib/cmdargs/dist/cmdargs.jar:$dir/dist/osmrmhvlib.jar:$dir/dist/osmhv.jar" de.cdauth.osm.historyviewer.Main "$@"
-fi
